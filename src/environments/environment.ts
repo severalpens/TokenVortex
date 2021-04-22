b65639d9dda316fe1b095d59923a3313e941b624
Paul C <paulcollinsaf@gmail.com>
2021-04-22 21:34:01 +1000

    change environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-
+  production: true,
   apiDomain: "https://tokenvortexapi.herokuapp.com",
   apiUrl: "https://tokenvortexapi.herokuapp.com/tokenvortex", 
 //  apiUrl: "http://localhost:9002/tokenvortex",
